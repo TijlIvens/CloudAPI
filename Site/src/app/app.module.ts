03c8ce54b1d6feed0fdef10096c3fd98ecd88381
TijlIvens <tijl.ivens@gmail.com>
2019-06-02 20:54:49 +0200

    Security update

--- Site/src/app/app.module.ts
+++ Site/src/app/app.module.ts
@@ -17,18 +17,14 @@ import { MainListComponent } from './main-list/main-list.component';
 import { HeaderComponent } from './header/header.component';
 
 import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
-import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
 import { LoginComponent } from './login/login.component';
 
 let config = new AuthServiceConfig([
   {
     id: GoogleLoginProvider.PROVIDER_ID,
     provider: new GoogleLoginProvider("704481721683-d5pnn60fdgc3memf2ocu21540bjs3ivl.apps.googleusercontent.com")
-  },
-  /*{
-    id: FacebookLoginProvider.PROVIDER_ID,
-    provider: new FacebookLoginProvider("325904254691023|QaBvFJKDJ8rR5s82HfLU8wkOg5o")
-  }*/
+  }
 ]);
 
 export function provideConfig() {
